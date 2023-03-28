ddbd2f2cf5d327f150ab233ca415f70a6ab458bc
gokulpr-47 <90414736+shreyareddy2407@users.noreply.github.com>
2023-03-29 03:39:57 +0530

    added routes for login

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -34,3 +34,51 @@ export const CreateUser = async (req, res) => {
     });
   }
 };
+
+export const Login = async (req, res) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (!existingUser) {
+      return res.send({
+        message: "User does not exist",
+        success: false,
+        data: null,
+      });
+    }
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      existingUser.password
+    );
+    if (!isPasswordCorrect) {
+      return res.send({
+        message: "Incorrect password",
+        success: false,
+        data: null,
+      });
+    }
+
+    const token = jwt.sign(
+      { userId: existingUser._id },
+      process.env.jwt_secret,
+      {
+        expiresIn: "1h",
+      }
+    );
+    res.render("dashboard", {
+      existingUser,
+      userId: existingUser._id,
+    });
+    // res.send({
+    //   message: "User logged in successfully",
+    //   success: true,
+    //   data: token,
+    //   user: existingUser,
+    // });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+      data: null,
+    });
+  }
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express();
-import { CreateUser } from "../Controllers/authController.js";
+import { CreateUser, Login, signup } from "../Controllers/authController.js";
 
+router.get("/signup", signup);
 router.post("/create-user", CreateUser);
+router.post("/login", Login);
 
 export default router;
