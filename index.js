40141c6fb72023c81e1c87141d04a0ec0e8c69e8
gokulpr-47 <90414736+shreyareddy2407@users.noreply.github.com>
2023-03-29 18:09:39 +0530

    added routes

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from "express";
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3002, () => {
+import authRoutes from "./routes/authRoutes.js";
+import usersRoutes from "./routes/usersRoutes.js";
+import usageRoutes from "./routes/usageRoutes.js";
+
+app.use("/api/users", usersRoutes); //get all users
+app.use("/api/auth", authRoutes);
+app.use("/api/usersRoutes", usersRoutes); //get current user
+app.use("/api/usage", usageRoutes); //send the usage from the form
+
+app.listen(port, () => {
   console.log("server listening to port 3000");
 });
