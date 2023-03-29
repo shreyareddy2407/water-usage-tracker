f2a77d4eb86c592b0b427681ed7ab32a93fabcfa
gokulpr-47 <90414736+shreyareddy2407@users.noreply.github.com>
2023-03-29 18:30:28 +0530

    updates

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
+// import dbConfig from "./config/dbConfig.js";
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// parse application/x-www-form-urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 import authRoutes from "./routes/authRoutes.js";
-import usersRoutes from "./routes/usersRoutes.js";
+// import usersRoutes from "./routes/usersRoutes.js";
 import usageRoutes from "./routes/usageRoutes.js";
 
-app.use("/api/users", usersRoutes); //get all users
+// app.use("/api/users", usersRoutes); //get all users
 app.use("/api/auth", authRoutes);
-app.use("/api/usersRoutes", usersRoutes); //get current user
+// app.use("/api/usersRoutes", usersRoutes); //get current user
 app.use("/api/usage", usageRoutes); //send the usage from the form
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.listen(port, () => {
   console.log("server listening to port 3000");
